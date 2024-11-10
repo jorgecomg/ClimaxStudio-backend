4379bcf9e6820a56ab1c4938a14599fe59d4bb9b
george <gege.g.r@hotmail.com>
2024-11-10 16:44:21 -0300

    .

--- src/validation/evento/eventoValidation.js
+++ src/validation/evento/eventoValidation.js
@@ -1,3 +1,23 @@
 export async function validarNovoEvento(eventoObj) {
+    if (!eventoObj.nome)
+        throw new Error('Nome do evento obrigatório')
+
+    if (!eventoObj.email)
+        throw new Error('Email obrigatório')
+
+    if (!eventoObj.telefone)
+        throw new Error('Telefone obrigatório')
+
+    if (!eventoObj.data)
+        throw new Error('Data do evento obrigatório')
+
+    if (!eventoObj.local)
+        throw new Error('Local do evento obrigatório')
+
+    if (!eventoObj.mensagem)
+        throw new Error('Descrição do evento obrigatório')
+
+    if (!eventoObj.pacote)
+        throw new Error('Pacote obrigatório')
 
 }
