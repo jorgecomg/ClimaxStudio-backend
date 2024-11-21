c7cba9321ecf73cf026923f0d107e20b35614b98
George Ramos <102702453+jorgecomg@users.noreply.github.com>
2024-11-20 22:20:29 -0300

    Update eventoController.js

--- src/controller/eventoController.js
+++ src/controller/eventoController.js
@@ -4,6 +4,8 @@ const endpoints = Router();
 import salvarEventoService from "../service/evento/salvarEventoService.js";
 import consultarEventosService from "../service/evento/consultarEventosService.js";
 import deletarEventosService from "../service/evento/deletarEventoService.js";
+import editarEventoService from "../service/evento/editarEventoService.js";
+
 
 endpoints.post('/evento', async (req, resp) => {
     try {
@@ -50,5 +52,19 @@ endpoints.delete('/evento/:id', async (req, resp) => {
         resp.status(400).send(criarErro(error))
     }
 })
+
+endpoints.put('/evento/:id', async (req, resp) => {
+    try {
+        const id = req.params.id;
+        const eventoData = req.body; 
+
+        await editarEventoService(id, eventoData);
+
+        resp.status(200).send({ message: 'Evento atualizado com sucesso!' });
+    } catch (error) {
+        logErro(error);
+        resp.status(400).send(criarErro(error)); 
+    }
+});
 
 export default endpoints;
