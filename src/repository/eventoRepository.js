6045aec1b166c02ccc60764fdecc44ad0573f216
George Ramos <102702453+jorgecomg@users.noreply.github.com>
2024-11-20 22:22:01 -0300

    Update eventoRepository.js

--- src/repository/eventoRepository.js
+++ src/repository/eventoRepository.js
@@ -33,3 +33,34 @@ export async function deletarEventos(id) {
     let linhasAfetadas = info.affectedRows;
     return linhasAfetadas;
 }
+
+export async function editarEvento(id, evento) {
+    const comando = `
+        UPDATE eventos
+        SET 
+            nome = ?, 
+            email = ?, 
+            telefone = ?, 
+            data = ?, 
+            local = ?, 
+            mensagem = ?, 
+            pacote = ?
+        WHERE id = ?;
+    `;
+
+    const resposta = await con.query(comando, [
+        evento.nome,
+        evento.email,
+        evento.telefone,
+        evento.data,
+        evento.local,
+        evento.mensagem,
+        evento.pacote,
+        id,
+    ]);
+
+    const info = resposta[0];
+    if (info.affectedRows === 0) {
+        throw new Error("Evento não encontrado ou não foi possível atualizar.");
+    }
+}
